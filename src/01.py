1f9cade4851e3c2c8acdd969cc4d218fc3b98f6f
wang shuwen <wangshuwen@yit.com>
2019-11-05 08:50:19 +0800

    first commit

---
+++ src/01.py
@@ -0,0 +1,65 @@
+# Fibonacci series:
+def fiboonacci(n):
+    """ print a fibonacci series up to n """
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+
+f100 = fiboonacci(100)
+f100
+
+
+def default_params(a, l=[]):
+    l.append(a)
+    print(l)
+
+
+default_params(1)
+default_params(2)
+default_params(3)
+
+
+def default_params2(a, l=None):
+    if l is None:
+        l = []
+    l.append(a)
+    print(l)
+
+
+default_params2(1)
+default_params2(2)
+default_params2(3)
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    print('args' + "-" * 40)
+
+    for arg in arguments:
+        print(arg)
+
+    print('keywords' + "-" * 40)
+
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
+
+
+def my_function():
+    """Do nothing, but document it.
+    No, really, it doesn't do anything.
+    """
+    pass
+
+
+print(my_function.__doc__)
