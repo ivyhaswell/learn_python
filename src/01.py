ade822b729f91c72b9d889716ad447ef9405c85d
wang shuwen <wangshuwen@yit.com>
2019-11-12 16:10:57 +0800

    11

--- src/01.py
+++ src/01.py
@@ -80,3 +80,48 @@ print(dict1.get(tk2))
 
 import builtins
 dir(builtins)
+
+
+class A:
+    a='a'
+
+class B:
+    a = 'b'
+
+class C(B,A):
+    pass
+
+ci = C()
+print(ci.a)
+
+import os
+help(os)
+
+import datetime
+today = datetime.date.today()
+bd = datetime.date(1993,5,25)
+today - bd
+
+def average(values):
+    """Computes the arithmetic mean of a list of numbers.
+
+    >>> print(average([20, 30, 70]))
+    40.0
+    """
+    return sum(values) / len(values)
+
+import doctest
+doctest.testmod()   # automatically validate the embedded tests
+
+import unittest
+
+class TestStatisticalFunctions(unittest.TestCase):
+
+    def test_average(self):
+        self.assertEqual(average([20, 30, 70]), 40.0)
+        self.assertEqual(round(average([1, 5, 7]), 1), 4.3)
+        with self.assertRaises(ZeroDivisionError):
+            average([])
+        with self.assertRaises(TypeError):
+            average(20, 30, 70)
+unittest.main()  # Calling from the command line invokes all tests
